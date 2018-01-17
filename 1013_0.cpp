a8549a889695f2737eb56d384259f11cea53325c
Liwei Kang <804093559@qq.com>
2018-01-17 11:06:11 +0800

    DP
    网上的解法，不很理解，要再看

---
+++ 1013_0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int V, N, value[1000], volumn[1000], bag[10000] = { 0 };
+	scanf("%d %d", &V, &N);
+	for (int i = 0; i < N; ++i)
+		scanf("%d %d", &volumn[i], &value[i]);
+
+	for (int v = 1; v <= V; ++v)
+		for (int i = 0; i < N; ++i)
+			if (volumn[i] <= v)
+				bag[v] = max(bag[v - volumn[i]] + value[i], bag[v]);
+	printf("%d", bag[V]);
+}
---
+++ 1013_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int max(int a, int b) {
+	if (a > b) return a;
+	else return b;
+}
+
+int main() {
+	int V, N, c[1001], w[1001], f[10001];
+	scanf("%d", &V);
+	scanf("%d", &N);
+	int i, v;
+	for (i = 1; i <= N; i++) {
+		scanf("%d", &c[i]);
+		scanf("%d", &w[i]);
+	}
+	for (v = 0; v <= V; v++) {
+		f[v] = 0;
+	}
+	for (i = 1; i <= N; i++) {
+		for (v = c[i]; v <= V; v++) {//起点是c[i]，而且是从c[i]开始正序循环，递增循环，正序循环表示c[i]的数量有无限个！！
+
+			f[v] = max(f[v], f[v - c[i]] + w[i]);//这里通过递归f[v-c[i]]一定是最优解，如果加上w[i]权值比f[v]大，那么就是最优解。
+		}
+	}
+
+	printf("%d", f[V]);//完成递归以后f[V]就是最大权值
+
+
+	return 0;
+}
