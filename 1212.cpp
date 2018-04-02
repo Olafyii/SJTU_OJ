206b29b449405a67d288877008faab8a9a2069f1
Liwei Kang <804093559@qq.com>
2018-04-02 21:23:15 +0800

    二叉树的层次遍历
    注意main函数里val数组是申请的动态数组，不然会因为开了太多空间而stack overflow

---
+++ 1212.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+
+using namespace std;
+
+template <class elemType>
+class queue
+{
+public:
+	virtual bool isEmpty() const = 0;
+	virtual void enQueue(const elemType &x) = 0;
+	virtual elemType deQueue() = 0;
+	virtual elemType getHead() const = 0;
+	virtual ~queue() {}
+};
+
+template <class elemType>
+class linkQueue :public queue<elemType>
+{
+private:
+	struct node
+	{
+		elemType data;
+		node *next;
+		node(const elemType &x, node *N = NULL) :data(x), next(N) {}
+		node() :next(NULL) {}
+		~node() {}
+	};
+
+	node *front, *rear;
+
+public:
+	linkQueue();
+	~linkQueue();
+	bool isEmpty() const;
+	void enQueue(const elemType &x);
+	elemType deQueue();
+	elemType getHead() const;
+};
+
+template <class elemType>
+linkQueue<elemType>::linkQueue()
+{
+	front = rear = NULL;
+}
+
+template <class elemType>
+linkQueue<elemType>::~linkQueue()
+{
+	node *tmp;
+	while (front != NULL)
+	{
+		tmp = front;
+		front = tmp->next;
+		delete tmp;
+	}
+}
+
+template <class elemType>
+bool linkQueue<elemType>::isEmpty() const
+{
+	return front == NULL;
+}
+
+template <class elemType>
+elemType linkQueue<elemType>::getHead() const
+{
+	return front->data;
+}
+
+template <class elemType>
+void linkQueue<elemType>::enQueue(const elemType &x)
+{
+	if (rear == NULL)
+		front = rear = new node(x);
+	else
+		rear = rear->next = new node(x);
+}
+
+template <class elemType>
+elemType linkQueue<elemType>::deQueue()
+{
+	node *tmp = front;
+	elemType value = front->data;
+	front = front->next;
+	if (front == NULL)
+		rear = NULL;
+	delete tmp;
+	return value;
+}
+
+struct Pair//仅用于记忆节点关系
+{
+	int left;
+	int right;
+	Pair(int l = 0, int r = 0) :left(l), right(r) {}
+};
+
+class binaryTree
+{
+private:
+	struct node
+	{
+		int data;
+		node *left, *right;
+		node() :left(NULL), right(NULL) {}
+		node(int item, node *l = NULL, node* r = NULL) :data(item), left(l), right(r) {}
+		~node() {}
+	};
+
+	node *root;
+	node *help(Pair data[], int val[], int r)
+	{
+		node *tmp = new node(val[r]);
+		if (data[r].left == 0 && data[r].right == 0)
+			return tmp;
+		if (data[r].left != 0)
+			tmp->left = help(data, val, data[r].left);
+		if (data[r].right != 0)
+			tmp->right = help(data, val, data[r].right);
+		return tmp;
+	}
+
+public:
+	binaryTree() :root(NULL) {}
+	~binaryTree()
+	{
+		clear(root);
+	}
+	void clear(node *r)
+	{
+		if (r == NULL)
+			return;
+		clear(r->left);
+		clear(r->right);
+		delete r;
+		r = NULL;
+	}
+	void createTree(Pair data[], int val[], int r)
+	{
+		root = help(data, val, r);
+	}
+	bool isComplete()
+	{
+		linkQueue<node *> q;
+		q.enQueue(root);
+		bool mustHaveNoChild = false, res = true;
+		while (!q.isEmpty())
+		{
+			node *tmp = q.deQueue();
+			if (mustHaveNoChild)
+			{
+				if (tmp->left != NULL || tmp->right != NULL)
+				{
+					res = false;
+					break;
+				}
+			}
+			else
+			{
+				if (tmp->left != NULL && tmp->right != NULL)
+				{
+					q.enQueue(tmp->left);
+					q.enQueue(tmp->right);
+				}
+				else if (tmp->left == NULL && tmp->right == NULL)
+				{
+					mustHaveNoChild = true;
+					q.enQueue(tmp->left);
+				}
+				else if (tmp->left == NULL && tmp->right != NULL)
+				{
+					res = false;
+					break;
+				}
+				else
+				{
+					mustHaveNoChild = true;
+				}
+			}
+		}
+		return res;
+	}
+	void BFS()
+	{
+		linkQueue<node *> q;
+		q.enQueue(root);
+		while (!q.isEmpty())
+		{
+			node *tmp = q.deQueue();
+			printf("%d ", tmp->data);
+			if (tmp->left != NULL)
+				q.enQueue(tmp->left);
+			if (tmp->right != NULL)
+				q.enQueue(tmp->right);
+		}
+	}
+};
+
+int main()
+{
+	Pair data[100000];
+	int N, v, root;
+	int *val = new int[100000];
+	int pi, qi;
+	bool notRoot[100000] = { 0 };
+	scanf("%d", &N);
+	for (int i = 1; i < N + 1; ++i)
+	{
+		scanf("%d %d %d", &pi, &qi, &v);
+		val[i] = v;
+		data[i] = Pair(pi, qi);
+		notRoot[pi] = 1;
+		notRoot[qi] = 1;
+	}
+
+	for (int i = 1; i < N + 1; ++i)
+		if (notRoot[i] == 0)
+			root = i;
+
+	binaryTree bTree;
+	bTree.createTree(data, val, root);
+	bTree.BFS();
+	delete val;
+	return 0;
+}
